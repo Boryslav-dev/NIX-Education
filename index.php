f8441630a6d80451603be33a4416160867b89879
Boryslav-Dev <kuchkovborislav000@gmail.com>
2021-01-30 14:48:12 +0200

    first try

---
+++ index.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href = "style.css">
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,400,600,700&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+    <title>FineCalculator</title>
+</head>
+<body>
+    <!-- *********  Header  ********** -->
+    <header>
+        <div id = "header">
+            <h1><a href="index.php"><b>NIX</b>Education</a></h1>
+            <div id = "nav">
+                <ul>
+                    <li id = "task_1"><a href = "">Урок 1</a></li>
+                    <li id = "task_2"><a href = "">Урок 2</a>
+                    <li id = "task_3"><a href = "">Урок 3</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+    <!-- *********  Content  ********** -->
+    <div id = "content">
+        <div class="info-content">
+
+        </div>
+
+        <div class="info-content">
+
+        </div>
+
+        <div class="info-content">
+
+        </div>
+</body>
+    <footer>
+        <div id = "footer">
+
+        </div>
+    </footer>
+</html>
